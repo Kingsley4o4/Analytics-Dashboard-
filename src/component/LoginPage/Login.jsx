79977930f28badc6efe63bb7ea4909c7f1449d13
Kingsley <olorunfemiolakunle44@gmail.com>
2026-03-01 05:58:10 +0100

    UserProfile

---
+++ src/component/LoginPage/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./login.css"
+
+function Login() {
+  return (
+    <div className="Login">
+        <div className="loginWrapper">
+        <div className="modial"> 
+
+
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
